b2e9c02e222940adb8774423972448c00b73a40c
Mantecorso <luisjuradoquesada@gmail.com>
2018-06-15 10:10:52 +0200

    maximunAge, timeout y enableHighAccuracy

--- public/main.js
+++ public/main.js
@@ -11,6 +11,12 @@ if("geolocation" in navigator) {
 if("geolocation" in navigator) {
 navigator.geolocation.getCurrentPosition(function(position){
     console.log(position);
+}, function (err) {
+    console.log(err);
+},{
+    maximunAge: Infinity, //milisegundos tambien se puede poner tiempo infinito desde la ultima vez que tiene en cache
+    timeout: 10000, //milisegundos tiempo de espera para encontrar la geolocalizacion
+    enableHighAccuracy: true // alta precision
 });
 } else {
     alert('La geolocalizacion no existe en su ordenador');
