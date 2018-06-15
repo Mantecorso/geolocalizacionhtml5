0cac772960a0fe07495f214bd8acce5c4590883e
Mantecorso <luisjuradoquesada@gmail.com>
2018-06-15 09:58:27 +0200

    usa getCurrentPosition para buscar la posicion

--- public/main.js
+++ public/main.js
@@ -5,3 +5,13 @@ if("geolocation" in navigator) {
 } else {
     alert('La geolocalizacion no existe en su ordenador');
 }
+
+// getCurrentPosition() te da la posicion
+
+if("geolocation" in navigator) {
+navigator.geolocation.getCurrentPosition(function(position){
+    console.log(position);
+});
+} else {
+    alert('La geolocalizacion no existe en su ordenador');
+}
